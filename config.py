5839582be3dae8113e9accc1a55df360e70a4d3a
Chen Wen Kang <cwk1998@hotmail.com>
2020-03-30 07:59:42 +0800

    Initial Commit

---
+++ config.py
@@ -0,0 +1,13 @@
+# Training settings
+class Config:
+    batch_size: int = 512
+    test_batch_size: int = 1000
+    epochs: int = 10
+    lr: float = 0.01
+    momentum: float = 0.9
+    no_cuda: bool = False
+    seed: int = 1
+    log_interval: int = 10
+    save_model: bool = False
+    data_dir: str ="./cache/data/"
+    output_dir: str = "./cache/models/"
