Fix incorrect dimension issue on feature map for LeNet5 halfed

models/lenet5.py
@@ -59,7 +59,7 @@ class LeNet5Halfed(nn.Module):
         output = self.maxpool2(output)
         output = self.conv3(output)
         output = self.relu3(output)
-        feature = output.view(-1, 120)
+        feature = output.view(-1, 60)
         output = self.fc1(feature)
         output = self.relu4(output)
         output = self.fc2(output)