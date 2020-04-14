6ca5f5395b91089b233521a8655c3caf7f0c7071
Chen Wen Kang <cwk1998@hotmail.com>
2020-04-15 04:22:36 +0800

    Change script to invoke training sequentially instead of parallelly

--- train_all_source_networks.bat
+++ train_all_source_networks.bat
@@ -1,11 +1,11 @@
 call .venv/Scripts/activate
 
-start python train_source_networks.py --dataset first5_mnist
-start python train_source_networks.py --dataset last5_mnist
-start python train_source_networks.py --dataset mnist
-start python train_source_networks.py --dataset cifar10
+python train_source_networks.py --dataset first5_mnist &
+python train_source_networks.py --dataset last5_mnist &
+python train_source_networks.py --dataset mnist &
+python train_source_networks.py --dataset cifar10 &
 
-start python train_source_networks.py --dataset first5_mnist --arch lenet5_halfed
-start python train_source_networks.py --dataset last5_mnist --arch lenet5_halfed
-start python train_source_networks.py --dataset mnist --arch lenet5_halfed
-start python train_source_networks.py --dataset cifar10 --arch lenet5_halfed
+python train_source_networks.py --dataset first5_mnist --arch lenet5_halfed &
+python train_source_networks.py --dataset last5_mnist --arch lenet5_halfed &
+python train_source_networks.py --dataset mnist --arch lenet5_halfed &
+python train_source_networks.py --dataset cifar10 --arch lenet5_halfed
