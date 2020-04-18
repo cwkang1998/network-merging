c76420b8fbce5eb61ae75ce8645f9fc86aec4c32
Chen Wen Kang <cwk1998@hotmail.com>
2020-04-18 22:07:39 +0800

    Add bash scripts for training source nets and running experiments

---
+++ run_experiments.sh
@@ -0,0 +1,7 @@
+source .venv/Scripts/activate
+
+python merge.py --dataset=disjoint_mnist --arch=lenet5 --experiment=logits_statistics &&
+python merge.py --dataset=mnist_cifar10 --arch=lenet5 --experiment=logits_statistics &&
+
+python merge.py --dataset=disjoint_mnist --arch=lenet5_halfed --experiment=logits_statistics &&
+python merge.py --dataset=mnist_cifar10 --arch=lenet5_halfed --experiment=logits_statistics &&
