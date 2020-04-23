ef13e07dfd666c133a620a202f0091bd5b274a8c
Chen Wen Kang <cwk1998@hotmail.com>
2020-04-24 00:53:23 +0800

    Implement generative adversarial network as an available architecture

---
+++ archs/gan.py
@@ -0,0 +1,34 @@
+from torch import nn
+
+
+class Generator(nn.Module):
+    def __init__(self, img_size, latent_dim, channels=3):
+        super(Generator, self).__init__()
+
+        self.init_size = img_size // 4
+        self.l1 = nn.Sequential(nn.Linear(latent_dim, 128 * self.init_size ** 2))
+
+        self.conv_blocks0 = nn.Sequential(nn.BatchNorm2d(128),)
+        self.conv_blocks1 = nn.Sequential(
+            nn.Conv2d(128, 128, 3, stride=1, padding=1),
+            nn.BatchNorm2d(128, 0.8),
+            nn.LeakyReLU(0.2, inplace=True),
+        )
+        self.conv_blocks2 = nn.Sequential(
+            nn.Conv2d(128, 64, 3, stride=1, padding=1),
+            nn.BatchNorm2d(64, 0.8),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(64, channels, 3, stride=1, padding=1),
+            nn.Tanh(),
+            nn.BatchNorm2d(channels, affine=False),
+        )
+
+    def forward(self, z):
+        out = self.l1(z)
+        out = out.view(out.shape[0], 128, self.init_size, self.init_size)
+        img = self.conv_blocks0(out)
+        img = nn.functional.interpolate(img, scale_factor=2)
+        img = self.conv_blocks1(img)
+        img = nn.functional.interpolate(img, scale_factor=2)
+        img = self.conv_blocks2(img)
+        return img
